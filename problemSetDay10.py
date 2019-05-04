fbb58988cc4d84dd0d58682c73c1912f1f1d21f8
Tallulahwalz <tmwalznyc@gmail.com>
2019-05-04 16:22:46 +0000

    Final Project

--- problemSetDay10.py
+++ problemSetDay10.py
@@ -1,1 +1,38 @@
-# File name: problemSetDay10.py
+# File name: problemSetDay9.py
+import random
+
+wordList = ["whatup", "bird", "pink", "orange","england", "park"]
+randomword = wordList[random.randint(0,4)]
+
+characters= list(randomword)
+display = list()
+displaylength = len(characters)
+
+for i in range (0, len(characters)):
+  display.append("_")
+print(display)
+
+tracker = 0
+userGuess = input("Guess a letter!")
+
+while tracker <= 5:
+  if userGuess in display:
+    print("You already guessed this letter try again!!!")
+  if userGuess in characters:
+    index = characters.index(userGuess)
+    display[index] = userGuess
+    print(display)
+    if display == randomword:
+      print ('yay you got a letter')
+
+  else:
+    tracker += 1
+    print(tracker)
+    print ('that letter is not in this word, try again')
+
+  if tracker == 5:
+    print("You maxed out on guesses. You loose!")
+
+  userGuess = input("Guess a letter!")
+
+  print('Yay you guessed the word!')
